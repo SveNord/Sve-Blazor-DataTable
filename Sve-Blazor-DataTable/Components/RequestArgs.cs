b67c7cbc3dece42e4ab22a5cb677e399aaf83261
Sven Matthys <matthys.sven@gmail.com>
2020-05-25 20:17:28 +0200

    Initial commit

---
+++ Sve-Blazor-DataTable/Components/RequestArgs.cs
@@ -0,0 +1,25 @@
+﻿using Sve.Blazor.Core.Models;
+
+namespace Sve.Blazor.DataTable.Components
+{
+    public class RequestArgs
+    {
+        public int PageNr { get; set; }
+
+        public int PageSize { get; set; }
+
+        public SortDirection SortDirection { get; set; }
+
+        public string SortColumn { get; set; }
+
+        // TODO: Add filters
+
+        public RequestArgs(int pageNr, int pageSize, SortDirection sortDirection, string sortColumn)
+        {
+            PageNr = pageNr;
+            PageSize = pageSize;
+            SortDirection = sortDirection;
+            SortColumn = sortColumn;
+        }
+    }
+}
---
+++ Sve-Blazor-DataTable/Components/TableStyle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Sve.Blazor.DataTable.Components
+{
+    [Flags]
+    public enum TableStyle
+    {
+        Dark = 1,
+        Striped = 2,
+        Bordered = 4,
+        Borderless = 8,
+        Hover = 16,
+        Sm = 32,
+    }
+}
