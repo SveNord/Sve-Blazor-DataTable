09ffff02795330377e23da2ba563cf3ccf68bd1a
Sven Matthys <matthys.sven@gmail.com>
2020-06-04 21:51:25 +0200

    Version 1.0.0-alpha

--- Sve-Blazor-DataTable-Examples/Data/WeatherForecastService.cs
+++ Sve-Blazor-DataTable-Examples/Data/WeatherForecastService.cs
@@ -1,5 +1,9 @@
+using Microsoft.VisualBasic.CompilerServices;
+using Sve.Blazor.Core.Models;
+using Sve.Blazor.DataTable.Components;
 using System;
 using System.Linq;
+using System.Linq.Dynamic.Core;
 using System.Threading.Tasks;
 
 namespace Sve.Blazor.DataTable.Examples.Data
@@ -11,11 +15,11 @@ namespace Sve.Blazor.DataTable.Examples.Data
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+        public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate, int amount = 10)
         {
             var rng = new Random();
 
-            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Task.FromResult(Enumerable.Range(1, amount).Select(index => new WeatherForecast
             {
                 Date = startDate.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
@@ -23,6 +27,44 @@ namespace Sve.Blazor.DataTable.Examples.Data
                 MyNullableInt = rng.Next(1, 10) > 3 ? (int?)null : 1,
                 Country = (Country) Enum.GetValues(typeof(Country)).GetValue(rng.Next(0, Enum.GetValues(typeof(Country)).Length))
             }).ToArray());
+        }
+
+        private WeatherForecast[]? generatedForecasts = null;
+        public Task<Sve.Blazor.Core.Models.PagedResult<WeatherForecast>> SearchForecastAsync(RequestArgs<WeatherForecast> args, int amount = 10)
+        {
+            Sve.Blazor.Core.Models.PagedResult<WeatherForecast> pagedResult = null;
+            Pager pager = null;
+
+            if (generatedForecasts == null)
+            {
+                var rng = new Random();
+
+                generatedForecasts = Enumerable.Range(1, amount).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(-10).AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)],
+                    MyNullableInt = rng.Next(1, 10) > 3 ? (int?)null : 1,
+                    Country = (Country)Enum.GetValues(typeof(Country)).GetValue(rng.Next(0, Enum.GetValues(typeof(Country)).Length))
+                }).ToArray();
+
+                pager = new Pager(pageNr: 1, pageSize: 10, sortColumn: "", SortDirection.Ascending);
+            }
+            else pager = args.Pager;
+
+            IQueryable<WeatherForecast> result = generatedForecasts.AsQueryable();
+
+            if(args != null)
+            {
+                foreach (var filter in args.AppliedFilters)
+                {
+                    result = result.Where(filter.GenerateExpression());
+                }
+            }
+
+            pagedResult = Sve.Blazor.Core.Utils.ApplyPaging(result, pager);
+
+            return Task.FromResult(pagedResult);
         }
     }
 }
--- Sve-Blazor-DataTable/Components/RequestArgs.cs
+++ Sve-Blazor-DataTable/Components/RequestArgs.cs
@@ -5,22 +5,27 @@ namespace Sve.Blazor.DataTable.Components
 {
     public class RequestArgs<TModel>
     {
-        public int PageNr { get; set; }
+        //public int PageNr { get; set; }
 
-        public int PageSize { get; set; }
+        //public int PageSize { get; set; }
 
-        public SortDirection SortDirection { get; set; }
+        //public SortDirection SortDirection { get; set; }
 
-        public string SortColumn { get; set; }
+        //public string SortColumn { get; set; }
 
-        public IList<FilterRule<TModel>> AppliedFilters { get; set; }
+        public Pager Pager { get; private set; }
+
+        public IList<FilterRule<TModel>> AppliedFilters { get; private set; }
 
         public RequestArgs(int pageNr, int pageSize, SortDirection sortDirection, string sortColumn, IList<FilterRule<TModel>> appliedFilters)
         {
-            PageNr = pageNr;
-            PageSize = pageSize;
-            SortDirection = sortDirection;
-            SortColumn = sortColumn;
+            Pager = new Pager(pageNr, pageSize, sortColumn, sortDirection);
+            AppliedFilters = appliedFilters;
+        }
+
+        public RequestArgs(Pager pager, IList<FilterRule<TModel>> appliedFilters)
+        {
+            Pager = pager;
             AppliedFilters = appliedFilters;
         }
     }
